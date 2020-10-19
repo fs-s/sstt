c5bde9373dd4a8fa24901a57e5fb267a19de4d7f
fs-s <55434294+fs-s@users.noreply.github.com>
2020-10-19 13:26:30 +0300

    Add files via upload

---
+++ src/components/SecondaryNav.vue
@@ -0,0 +1,29 @@
+<template>
+  <div
+    class="flex bg-secondary-nav shadow-lg max-w-5xl mx-auto mt-10 text-sm font-light"
+  >
+    <sec-nav-button>Sinu Pood</sec-nav-button>
+    <sec-nav-button>Sirvi</sec-nav-button>
+    <sec-nav-button>Punktide Pood</sec-nav-button>
+    <sec-nav-button>Uudised</sec-nav-button>
+    <sec-nav-button>Labor</sec-nav-button>
+    <div
+      class="ml-auto self-center mr-1 border border-gray-800 bg-secondary-nav rounded-sm"
+    >
+      <input
+        type="text"
+        name="search"
+        class="leading-7 px-1 text-gray-300 font-light italic mt-1 bg-transparent"
+        placeholder="Otsi poest..."
+      />
+    </div>
+  </div>
+</template>
+<script>
+import SecNavButton from "./SecNavButton.vue";
+export default {
+  components: {
+    SecNavButton,
+  },
+};
+</script>
